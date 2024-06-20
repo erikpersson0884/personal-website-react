f53de83e4f1ac663286a235a4a879e30d6f9aa32
Erik <erikpersson0884@gmail.com>
2024-06-21 01:38:20 +0200

    changed vite config file to typescript

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,16 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',  // Bind to all network interfaces
-    port: process.env.PORT || 3000,
+    port: parseInt(process.env.PORT ?? '3001'),
+  },
+  //change port for production
+  preview: {
+    port: 3001,
   },
   plugins: [react()],
 })
